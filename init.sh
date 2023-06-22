9ff3e4bb5a8b38e39c5eab2934961eead6d7ad5c
Elad Birnboim <eladbirn@gmail.com>
2023-06-22 20:38:14 +0300

    move boto and python3 installation to init.sh

---
+++ init.sh
@@ -0,0 +1,10 @@
+brew install jq
+
+brew install python3
+
+pip3 install boto3
+
+curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
+sudo installer -pkg AWSCLIV2.pkg -target /
+
+aws configure
